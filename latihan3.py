1d6ae2193b655a6d8931a67fa9cfe22d8504d085
fergiawann <115530180+fergiawann@users.noreply.github.com>
2022-10-24 06:30:14 +0700

    update latihan3.py
    merubah file latihan3.py

--- latihan3.py
+++ latihan3.py
@@ -1,7 +1,17 @@
 # input nilai variable
-a=input("masukkan nilai a:8")
-b=input("masukkan nilai b:7")
+a=input("masukkan nilai a:")
+b=input("masukkan nilai b:")
 
 # cetak nilai variable
-print("Variable a:8",a)
-print("Variable b:7",b)
+print("Variable a:",a)
+print("Variable b:",b)
+
+# cetak hasil operasi kedua variable dengan String Format
+print("Hasil penggabungan {1}+{0}=%s".format(a,b) %(a+b))
+
+# konversi nilai variable
+a=int(a)
+b=int(b)
+print("Hasil penjumlahan {1}+{0}=%d".format(a,b) %(a+b))
+print("Hasil pembagian {1}/{0}=%d".format(a,b) %(a/b))
+
